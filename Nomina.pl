89f19b950f83c1ead8ec3eac18e2e39a65804baf
jmferreirab <jmferreirab@correo.udistrital.edu.co>
2017-10-24 13:05:56 -0500

    Add files via upload
    Finished and checked by Daniel.

--- Nomina.pl
+++ Nomina.pl
@@ -1,9 +1,25 @@
 %Descripcion de profesores
+profesor(nombre('Pablo'), tipoContrato('TCO'), status('Asistente')).
 profesor(nombre('Melissa'), tipoContrato('TCO'), status('Asistente')).
+profesor(nombre('Ped'), tipoContrato('HC'), status('Asistente')).
+profesor(nombre('Cindy'), tipoContrato('HC'), status('Asistente')).
+profesor(nombre('Maria'), tipoContrato('HCH'), status('Asistente')).
+profesor(nombre('Mario'), tipoContrato('MTO'), status('Asistente')).
+profesor(nombre('Natalia'), tipoContrato('PLANTA'), status('Asistente')).
+profesor(nombre('Pablo'), tipoContrato('TCO'), status('Titular')).
+profesor(nombre('Cindy'), tipoContrato('HC'), status('Titular')).
 
-%Pago base segun contrato y status
-baseHora(tipoContrato('TCO'), status('Asistente'), 10000).
-baseHora(tipoContrato('HC'), status('Asistente'), 1500).
+%Pago base hora segun contrato y status
+baseHora(tipoContrato('TCO'), status('Titular'), 10000).
+baseHora(tipoContrato('HC'), status('Titular'), 12000).
+baseHora(tipoContrato('HCH'), status('Titular'), 15000).
+baseHora(tipoContrato('MTO'), status('Titular'), 10000).
+baseHora(tipoContrato('PLANTA'), status('Titular'), 8000).
+baseHora(tipoContrato('TCO'), status('Asistente'), 9000).
+baseHora(tipoContrato('HC'), status('Asistente'), 10800).
+baseHora(tipoContrato('HCH'), status('Asistente'), 14500).
+baseHora(tipoContrato('MTO'), status('Asistente'), 9000).
+baseHora(tipoContrato('PLANTA'), status('Asistente'), 7200).
 
 %Horas semanalas segun tipo de contrato
 horasContrato(tipoContrato('TCO'),35).
@@ -13,13 +29,18 @@ horasContrato(tipoContrato('MTO'),22).
 horasContrato(tipoContrato('PLANTA'),40).
 
 bonoContrato(0.15, tipoContrato('TCO')).
+bonoContrato(0.20, tipoContrato('HC')).
+bonoContrato(0.25, tipoContrato('HCH')).
+bonoContrato(0.10, tipoContrato('MTO')).
+bonoContrato(0.08, tipoContrato('PLANTA')).
 
 %Semanas segun tipo de semestre
 tipoSemestre('Normal',16).
 tipoSemestre('Intersemestral',4).
 
 %Calculo de la nomina por semestre
-nominaSemestre(profesor(nombre(X), Y, Z), Semestre, Total) :- baseHora(Y, Z, B), 
+nominaSemestre(profesor(nombre(X), Y, Z), Semestre, Total) :- profesor(nombre(X),Y,Z),
+															baseHora(Y, Z, B), 
 															bonoContrato(Bono, Y), 
 															tipoSemestre(Semestre,S), 
 															horasContrato(Y, H), 
